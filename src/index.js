30e2993f8669008b475a464c406d89c13564e06a
Marvelous <86561662+marvcoolguy@users.noreply.github.com>
2025-09-01 15:41:30 +0100

    Debugging :(

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; // import from node_modules
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // import Bootstrap JS
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import { useState, useEffect } from "react";
+import NavBar from "./Components/Navbar";
+import Home from "./Components/Home";
+import AboutView from "./Components/About";
+import SearchView from "./Components/SearchView";
+import Footer from "./Components/Footer"; // optional if you have a footer
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter basename="/MovieBrowserApp">    
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  useEffect(() => {
+    if (searchText) {
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=YOUR_API_KEY&language=en-US&query=${searchText}&page=1&include_adult=false`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setSearchResults(data.results || []);
+        })
+        .catch((err) => console.error("Error fetching data:", err));
+    }
+  }, [searchText]);
+
+  return (
+    <div className="App d-flex flex-column min-vh-100">
+      <NavBar searchText={searchText} setSearchText={setSearchText} />
+      
+      <main className="flex-fill">
+        {/* Simple hash routing */}
+        {window.location.hash === "" || window.location.hash === "#/" ? (
+          <Home />
+        ) : window.location.hash === "#/about" ? (
+          <AboutView />
+        ) : window.location.hash.startsWith("#/search") ? (
+          <SearchView keyword={searchText} searchResults={searchResults} />
+        ) : (
+          <Home /> // fallback
+        )}
+      </main>
+
+      <Footer /> {/* optional footer */}
+    </div>
+  );
+}
+
+export default App;
